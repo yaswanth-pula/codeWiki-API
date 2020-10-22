db88d4722e57b3c05c23ad7cff514d404c56763b
yaswanth-pula <35864576+Yaswanth899@users.noreply.github.com>
2020-10-22 12:17:57 +0530

    Chain handling of methods of same route

--- app.js
+++ app.js
@@ -31,42 +31,80 @@ const articleSchema = {
 const Article = mongoose.model("Article",articleSchema);
 
 // GET-all Route
-app.get("/articles",(req,res)=>{
-    Article.find((err,resultDocs)=>{
-        if(!err){
-            res.json(resultDocs);
-        }   
-        else{
-            res.send(err);
-        }
-    });
-});
+// app.get("/articles",(req,res)=>{
+//     Article.find((err,resultDocs)=>{
+//         if(!err){
+//             res.json(resultDocs);
+//         }   
+//         else{
+//             res.send(err);
+//         }
+//     });
+// });
 
-// Post(one-article) Route
-app.post("/articles",(req,res)=>{
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newArticle.save((err)=>{
-        if(!err){
-            res.send("Article Post Sucessfull");
-        }
-        else{
-            res.send(err);
-        }
-    });
-});
+// // Post(one-article) Route
+// app.post("/articles",(req,res)=>{
+//     const newArticle = new Article({
+//         title: req.body.title,
+//         content: req.body.content
+//     });
+//     newArticle.save((err)=>{
+//         if(!err){
+//             res.send("Article Post Sucessfull");
+//         }
+//         else{
+//             res.send(err);
+//         }
+//     });
+// });
 
-// Delete(all) Route
-app.delete("/articles", (req,res)=>{
-    Article.deleteMany({},(err)=>{
-        if(!err)
-            res.send("Deleted Successfully.");
-        else
-            res.send(err);
-    });
-});
+// // Delete(all) Route
+// app.delete("/articles", (req,res)=>{
+//     Article.deleteMany({},(err)=>{
+//         if(!err)
+//             res.send("Deleted Successfully.");
+//         else
+//             res.send(err);
+//     });
+// });
+
+// chained route handling
+app.route("/articles")
+    // get method for /articles
+    .get((req,res)=>{
+            Article.find((err,resultDocs)=>{
+                if(!err){
+                    res.json(resultDocs);
+                }   
+                else{
+                    res.send(err);
+                }
+            });
+        })
+    // post method for /articles
+    .post((req,res)=>{
+            const newArticle = new Article({
+                title: req.body.title,
+                content: req.body.content
+            });
+            newArticle.save((err)=>{
+                if(!err){
+                    res.send("Article Post Sucessfull");
+                }
+                else{
+                    res.send(err);
+                }
+            });
+        })
+    // delete method for /articles    
+    .delete((req,res)=>{
+            Article.deleteMany({},(err)=>{
+                if(!err)
+                    res.send("Deleted Successfully.");
+                else
+                    res.send(err);
+            });
+        });
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is Up and Running");

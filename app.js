2b579102521cc13b3dd9478a744bea0dfda79480
yaswanth-pula <35864576+Yaswanth899@users.noreply.github.com>
2020-10-22 12:54:22 +0530

    Added Get method for specfic article

--- app.js
+++ app.js
@@ -30,45 +30,7 @@ const articleSchema = {
 // collection
 const Article = mongoose.model("Article",articleSchema);
 
-// GET-all Route
-// app.get("/articles",(req,res)=>{
-//     Article.find((err,resultDocs)=>{
-//         if(!err){
-//             res.json(resultDocs);
-//         }   
-//         else{
-//             res.send(err);
-//         }
-//     });
-// });
-
-// // Post(one-article) Route
-// app.post("/articles",(req,res)=>{
-//     const newArticle = new Article({
-//         title: req.body.title,
-//         content: req.body.content
-//     });
-//     newArticle.save((err)=>{
-//         if(!err){
-//             res.send("Article Post Sucessfull");
-//         }
-//         else{
-//             res.send(err);
-//         }
-//     });
-// });
-
-// // Delete(all) Route
-// app.delete("/articles", (req,res)=>{
-//     Article.deleteMany({},(err)=>{
-//         if(!err)
-//             res.send("Deleted Successfully.");
-//         else
-//             res.send(err);
-//     });
-// });
-
-// chained route handling
+///////////////////////////// chained route handling for All articles /////////////////////////
 app.route("/articles")
     // get method for /articles
     .get((req,res)=>{
@@ -105,6 +67,26 @@ app.route("/articles")
                     res.send(err);
             });
         });
+
+///////////////////////////// chained route handling for specific article /////////////////////////
+ 
+app.route("/articles/:articleTitle")
+    // get method for specific article.
+    .get((req,res)=>{
+        const reqTitle = req.params.articleTitle;
+        Article.findOne({title:reqTitle},(err,resultDoc)=>{
+            if(!err){
+                if(resultDoc)
+                    res.json(resultDoc);
+                else
+                    res.json("No Article resource found");
+            }   
+            else{
+                res.send(err);
+            }
+        });
+    });
+ 
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is Up and Running");
